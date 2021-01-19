fb414311f174271b09fa3f46b48a0aa72725099c
niccolosantelli <niccolo_santelli@live.it>
2021-01-19 17:18:23 +0100

    definizione navabar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    voceMenu : [
+      {
+        name: "Courses",
+      },
+      {
+        name: "Course Formats"
+      },
+      {
+        name: "Add Course"
+      },
+      {
+        name: "Pages"
+      },
+      {
+        name: "Demos"
+      }
+    ]
+  
 
+
+  }
+
+})
