31d787ba741e0b9b24a4ff42f4129e157a592f5a
niccolosantelli <niccolo_santelli@live.it>
2021-01-20 19:11:08 +0100

    card corsi con vuejs
    definite card corsi con vuejs

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,49 +33,49 @@ let app = new Vue({
 
       },
       {
-        img: 'assets/img/dj-1.jpeg',
-        titolo: "How to be a DJ? Make Electronic Music",
-        tipo: "Electronic",
+        img: 'assets/img/termo.png',
+        titolo: "Nvidia and UE4 Technologies Practice",
+        tipo: "Nvidia",
         difficolta: "Advanced",
         quantita: "8 Lectures",
         durata: "6 hours",
-
+        prezzo: "$56"
       },
       {
-        img: 'assets/img/dj-1.jpeg',
-        titolo: "How to be a DJ? Make Electronic Music",
-        tipo: "Electronic",
+        img: 'assets/img/gambe.jpeg',
+        titolo: "Fashion Photography from professional",
+        tipo: "Fashion",
         difficolta: "Advanced",
-        quantita: "8 Lectures",
+        quantita: "6 Lectures",
         durata: "6 hours",
-
+        prezzo: "$56"
       },
       {
-        img: 'assets/img/dj-1.jpeg',
-        titolo: "How to be a DJ? Make Electronic Music",
-        tipo: "Electronic",
-        difficolta: "Advanced",
-        quantita: "8 Lectures",
+        img: 'assets/img/band.jpeg',
+        titolo: "Design Instruments for Communication",
+        tipo: "Communication",
+        difficolta: "Intermediate",
+        quantita: "6 Lectures",
         durata: "6 hours",
-
+        prezzo: "$43"
       },
       {
-        img: 'assets/img/dj-1.jpeg',
-        titolo: "How to be a DJ? Make Electronic Music",
-        tipo: "Electronic",
-        difficolta: "Advanced",
-        quantita: "8 Lectures",
+        img: 'assets/img/foglio.jpg',
+        titolo: "Make your Concept Right and Beautiful",
+        tipo: "Art",
+        difficolta: "Intermediate",
+        quantita: "Lectures",
         durata: "6 hours",
-
+        prezzo: "$43"
       },
       {
-        img: 'assets/img/dj-1.jpeg',
-        titolo: "How to be a DJ? Make Electronic Music",
-        tipo: "Electronic",
-        difficolta: "Advanced",
-        quantita: "8 Lectures",
+        img: 'assets/img/bici.jpeg',
+        titolo: "Road Bike Manual or How to BE a Champion",
+        tipo: "Bicycling",
+        difficolta: "Beginner",
+        quantita: "6 Lectures",
         durata: "6 hours",
-
+        prezzo: "$29"
       },
     ],
 
